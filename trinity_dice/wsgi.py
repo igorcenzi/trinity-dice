0bf211105c5b95b2bdd8050786694cb57433f774
Igor Cenzi <igor.cenzi2@gmail.com>
2022-07-12 15:14:32 -0300

    chore: initial structure

---
+++ trinity_dice/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for trinity_dice project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trinity_dice.settings')
+
+application = get_wsgi_application()
