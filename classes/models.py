3922384647799d67e420a19f5ed7a6af7cb739e9
Luiz Antonio Motta <mottaluizantonio@gmail.com>
2022-07-13 18:30:47 -0300

    fix: uuid

--- classes/models.py
+++ classes/models.py
@@ -1,6 +1,9 @@
+import uuid
+
 from django.db import models
 
 
 class Class(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=50)
     description = models.TextField()
--- classes/serializers.py
+++ classes/serializers.py
@@ -7,3 +7,4 @@ class ClassesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Class
         fields = "__all__"
+        read_only_fields = ["id"]
