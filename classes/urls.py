ebbc28479e099c6a921e4de9510c1fd34a5e2a20
Luiz Antonio Motta <mottaluizantonio@gmail.com>
2022-07-14 09:21:41 -0300

    fix: urls: create and get class

--- classes/urls.py
+++ classes/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("classes/", views.ListCreateClassView.as_view()),
+    path(
+        "system/<str:system_id>/classes/",
+        views.ListCreateClassView.as_view(),
+    ),
     path("classes/<str:class_id>/", views.GetDeleteClassView.as_view()),
 ]
