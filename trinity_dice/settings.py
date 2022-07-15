b75d2f1a7b6f34332dcb5632e5a48703a6fdd33c
Victor Ramon <victorrgds02@gmail.com>
2022-07-15 16:04:58 -0300

    ops: database-url config

--- trinity_dice/settings.py
+++ trinity_dice/settings.py
@@ -103,13 +103,13 @@ DATABASES = {
     #     }
     # }
 
-# DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-# if DATABASE_URL:
-#     db_from_env = dj_database_url.config(
-#         default=DATABASE_URL, conn_max_age=500, ssl_require=True
-#     )
-#     DATABASES["default"].update(db_from_env)
+if DATABASE_URL:
+    db_from_env = dj_database_url.config(
+        default=DATABASE_URL, conn_max_age=500, ssl_require=True
+    )
+    DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
