f51768179cacaa8ac30b8f130a6c6de33ecce1ec
Victor Ramon <victorrgds02@gmail.com>
2022-07-18 11:04:40 -0300

    style: translated variables to english

--- integrations/executor.py
+++ integrations/executor.py
@@ -8,18 +8,18 @@ from .wpp import whats_msg
 
 """
 
-def integrate(players_phones):
-    nova_sala = DiceRoomGenerator()
-    nova_sala.open()
-    nova_sala.generate_room()
-    nova_sala.enter_room()
-    link = nova_sala.get_room_link()
 
-    nova_sala.close()
-    
+def integrate(players_phones):
+    new_room = DiceRoomGenerator()
+    new_room.open()
+    new_room.generate_room()
+    new_room.enter_room()
+    link = new_room.get_room_link()
+
+    new_room.close()
+
     for num in players_phones:
-        message = f'Olá, como vai? Sou o mestre da jornada que você entrou pelo Trinity Dice! Aqui está o link da nossa sala de lançamento de dados: {link}'
+        message = f"Olá, como vai? Sou o mestre da jornada que você entrou pelo Trinity Dice! Aqui está o link da nossa sala de lançamento de dados: {link}"
         whats_msg(num, message)
 
     return True
-
