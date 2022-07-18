65bce882bcf79e51ea006bd622f65cf7b717c00c
Anderson Pinto <74440689+andersonstpinto@users.noreply.github.com>
2022-07-17 21:33:29 -0300

    fix: system post now accepts multiple classes on creation

--- systems/serializers.py
+++ systems/serializers.py
@@ -1,14 +1,30 @@
 from rest_framework import serializers
 from django.shortcuts import get_object_or_404
 
-
+from classes.models import Class
 from .models import System
 # from classes.models import Class
 
+class SystemClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Class
+        fields = ['name']
+
 class SystemSerializer(serializers.ModelSerializer):
+    classes = SystemClassSerializer(many=True)
+
     class Meta:
         model = System
         fields = '__all__'
-        read_only_fields = ['id', 'classes']
+        read_only_fields = ['id']
         depth = 1
+
+    def create(self, validated_data: dict):
+        class_data = validated_data.pop("classes")
+        system = System.objects.create(**validated_data)
+        for klass in class_data:
+            my_class, _ = Class.objects.get_or_create(**klass)
+            system.classes.add(my_class)
+
+        return system
 
