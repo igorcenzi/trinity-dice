47b5500e5937e1beb972b58b1cdf9e1d09fbad01
Anderson Pinto <74440689+andersonstpinto@users.noreply.github.com>
2022-07-18 13:00:02 -0300

    feat: added created_at and journeys_amount system fields

--- systems/serializers.py
+++ systems/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from journeys.models import Journey
 from classes.models import Class
 from items.models import Item
 from .models import System
@@ -20,16 +21,22 @@ class SystemItemsSerializer(serializers.ModelSerializer):
 class SystemSerializer(serializers.ModelSerializer):
     classes = SystemClassSerializer(many=True)
     items = SystemItemsSerializer(many=True, read_only=True)
+    journeys_amount = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = System
-        fields = ["id", "name", "dice", "version", "classes", "items", "is_active"]
+        fields = '__all__'
 
     def create(self, validated_data: dict):
         class_data = validated_data.pop("classes")
         system = System.objects.create(**validated_data)
+
         for klass in class_data:
             my_class, _ = Class.objects.get_or_create(**klass)
             system.classes.add(my_class)
 
         return system
+
+    def get_journeys_amount(self, obj):
+       return Journey.objects.filter(system=obj.id).count()
+
