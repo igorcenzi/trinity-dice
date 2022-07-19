38da1c650e857f2ca3c789f773095ca20ad5cd09
pedroalcon09 <pedroalcong@hotmail.com>
2022-07-19 13:33:22 -0300

    heroku config
    Signed-off-by: pedroalcon09 <pedroalcong@hotmail.com>

--- trinity_dice/settings.py
+++ trinity_dice/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["trinity-dice-api.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['trinity-dice-api.herokuapp.com', 'localhost']
 
 # Application definition
 
