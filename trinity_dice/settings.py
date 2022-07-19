6d32c06f121e28841db05b79e0afe3b9080707b9
pedroalcon09 <pedroalcong@hotmail.com>
2022-07-19 12:58:27 -0300

    heroku config
    Signed-off-by: pedroalcon09 <pedroalcong@hotmail.com>

--- trinity_dice/settings.py
+++ trinity_dice/settings.py
@@ -18,13 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DISPLAY = os.getenv("DISPLAY")
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["trinity-dice-api.herokuapp.com", "localhost"]
-
+ALLOWED_HOSTS = ["trinity-dice-api.herokuapp.com", "localhost", "127.0.0.1"]
 
 # Application definition
 
